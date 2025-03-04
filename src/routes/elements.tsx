dbba95dc22828f51d050e64289920253b196559e
Kyaw Zin Thant <kyawthantver2@gmail.com>
2025-03-04 13:41:07 +0630

    :sparkles: feat(init project): route set up , shadcn set up

---
+++ src/routes/elements.tsx
@@ -0,0 +1,19 @@
+import { Suspense, lazy, ElementType } from 'react';
+
+const Loadable =
+  <Props extends Record<string, unknown>>(Component: ElementType) =>
+  (props: Props) => (
+    <Suspense
+      fallback={
+        <div className="w-full h-full flex justify-center items-center">
+          <p>Loading Component come here</p>
+        </div>
+      }
+    >
+      <Component {...props} />
+    </Suspense>
+  );
+
+// Auth routes
+
+export const Login = Loadable(lazy(() => import('../features/auth/Login')));
