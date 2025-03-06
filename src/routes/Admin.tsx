b7a71bc2ab1a6c81d53857f58ef38ddba5eb903f
Kyaw Zin Thant <kyawthantver2@gmail.com>
2025-03-06 15:14:10 +0630

    :bug: fix(config): eslint config

--- src/routes/Admin.tsx
+++ src/routes/Admin.tsx
@@ -1,13 +1,7 @@
 import { Navigate, useRoutes } from 'react-router-dom';
-import Home from './Home';
 
 export default function Admin() {
   return useRoutes([
-    {
-      path: '/',
-      element: <Home />,
-    },
-
     {
       path: '*',
       element: <Navigate to="/" replace />,
