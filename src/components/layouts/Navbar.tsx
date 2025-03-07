89e21534788e6c14a9318e3bb5e54e85084909d3
GibbyAung <winmyataung986@gmail.com>
2025-03-07 16:03:07 +0630

    Updated aboutus section and some button colors

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -47,7 +47,7 @@ const Navbar: React.FC = () => {
 
           <Link to="/cart" className="relative">
             <img src={cartIcon} className="w-5 min-w-5" alt="Cart" />
-            <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
+            <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-blue-800 text-white aspect-square rounded-full text-[8px]">
               10
             </p>
           </Link>
